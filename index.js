f35788c1b0a28a595746cd9d4bb214dc431a7a7a
benholloway <ben.holloway@3plearning.com>
2014-07-24 14:01:22 +1000

    initial implementation

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ var path = require('path');
 var through = require('through2');
 var throughPipes = require('through-pipes');
 var minimatch = require('minimatch');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var inject = require('gulp-inject');
+var slash = require('gulp-slash');
 
 module.exports = function(temp) {
+  'use strict';
   var sourceTracking = trackSources();
   var outputPath     = temp;
   return {
     sources: function() {
       return throughPipes(function(readable) {
         return readable
-          .pipe(plugins.slash())
+          .pipe(slash())
           .pipe(sourceTracking.before())
           .pipe(gulp.dest(temp))
-          .pipe(plugins.slash())
+          .pipe(slash())
           .pipe(sourceTracking.after());
       });
     },
@@ -30,22 +35,22 @@ module.exports = function(temp) {
       return adjustSourceMaps(sourceTracking);
     },
     injectAppJS: injectAppJS
-  }
-}
+  };
+};
 
 function transpileES6(outputPath) {
   return through.obj(function(file, encoding, done) {
     var stream   = this;
-    var cwd      = plugins.slash(file.cwd);
-    var relative = plugins.slash(file.relative);
-    var base     = plugins.slash(path.resolve(outputPath));
-    var filename = plugins.slash(path.basename(file.path));
+    var cwd      = slash(file.cwd);
+    var relative = slash(file.relative);
+    var base     = slash(path.resolve(outputPath));
+    var filename = slash(path.basename(file.path));
     var outFile  = base + '/' + filename;
     var outPath  = base + '/' + relative.replace(filename, '');
     var command  = [ 'traceur', '--source-maps', '--out', outFile, file.path ].join(' ');
     child.exec(command, { cwd: cwd }, function(error, stdout, stdin) {
       if (error) {
-        var pending = new plugins.util.File()
+        var pending = new gutil.File();
         pending.cwd          = cwd;
         pending.base         = outputPath;
         pending.path         = outFile;
@@ -55,8 +60,8 @@ function transpileES6(outputPath) {
       } else {
         gulp.src(outFile.replace(/\.js$/, '.*'))
           .pipe(gulp.dest(outPath))
-          .pipe(plugins.slash())
-          .pipe(plugins.semiflat(base))
+          .pipe(slash())
+          .pipe(semiflat(base))
           .on('data', function(file) {
             stream.push(file);
           }).on('end', function() {
@@ -86,7 +91,6 @@ function trackSources() {
       });
     },
     replace: function(text) {
-      var generalBase = [ ];
       for (var i = Math.min(before.length, after.length) - 1; i >= 0; i--) {
         var regexp = minimatch.makeRe(after[i], 'g');
         text = text.replace(regexp, before[i]);
@@ -145,7 +149,7 @@ function traceurErrorReporter(sourceTracker) {
       } else {
         message = errorText.replace(/Error\:\s*Command failed\:\s*/g, '');
       }
-      var normalised = plugins.slash(message);
+      var normalised = slash(message);
       var unmapped   = sourceTracker.replace(normalised);
       if (output.indexOf(unmapped) < 0) {
         output.push(unmapped);
@@ -166,10 +170,10 @@ function adjustSourceMaps(sourceTracker) {
   return through.obj(function(file, encoding, done) {
     if (path.extname(file.path) === '.map') {
       function adjust(candidate) {
-        var normalised = plugins.slash(candidate);
+        var normalised = slash(candidate);
         var unmapped = sourceTracker.replace(normalised);
         var rootRelative = '/' + path.relative(file.cwd, unmapped);
-        return plugins.slash(rootRelative);
+        return slash(rootRelative);
       }
       var sourceMap = JSON.parse(file.contents.toString());
       delete sourceMap.sourcesContent;
@@ -193,11 +197,11 @@ function adjustSourceMaps(sourceTracker) {
 function injectAppJS(htmlBase, jsBase) {
   return through.obj(function(file, encoding, done) {
     var stream    = this;
-    var jsFile    = plugins.slash(file.path).replace(htmlBase, jsBase).replace(/\.html?$/, '.js');
-    var jsSources = gulp.src(jsFile, { read: false }).pipe(plugins.slash());
+    var jsFile    = slash(file.path).replace(htmlBase, jsBase).replace(/\.html?$/, '.js');
+    var jsSources = gulp.src(jsFile, { read: false }).pipe(slash());
     throughPipes(function(readable) {
       return readable
-        .pipe(plugins.inject(jsSources));
+        .pipe(inject(jsSources));
     })
       .output(function(file) {
         stream.push(file);
